48b53f008032237201fd808840d4e267a2ca1919
Ronald Weber <ronaldxweber@gmail.com>
2025-01-26 18:36:30 +0100

    Day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use itertools::Itertools;
+use std::fmt;
 use std::fs;
 use std::path::PathBuf;
 
@@ -14,38 +15,67 @@ struct Equation {
     numbers: Vec<i64>,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Operator {
     Add,
     Mul,
+    Cat,
+}
+
+impl fmt::Display for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let out = match *self {
+            Operator::Add => "+",
+            Operator::Mul => "*",
+            Operator::Cat => "||",
+        };
+        write!(f, "{out}")
+    }
 }
 
 const ADD_MUL: [Operator; 2] = [Operator::Add, Operator::Mul];
+const ALL_OPS: [Operator; 3] = [Operator::Add, Operator::Mul, Operator::Cat];
 
+#[derive(Debug)]
 struct Calculator {
     accumulator: i64,
-    prev: Option<(Operator, i64)>,
 }
 
 impl Calculator {
     fn new(num: i64) -> Self {
-        Self { accumulator: num, prev: None }
+        Self { accumulator: num }
     }
 
     fn apply(&mut self, op: Operator, num: i64) {
-        self.finalize();
-        self.prev = Some((op, num));
-    }
-
-    fn finalize(&mut self) -> i64 {
-        if let Some(prev) = self.prev {
-            self.accumulator = match prev.0 {
-                Operator::Add => self.accumulator + prev.1,
-                Operator::Mul => self.accumulator * prev.1,
-            };
+        fn cat(left: i64, right: i64) -> i64 {
+            let mut multiplicator = 10;
+            while multiplicator < right {
+                multiplicator *= 10;
+            }
+            left * multiplicator + right
         }
-        self.accumulator
+        let new = match op {
+            Operator::Add => self.accumulator + num,
+            Operator::Mul => self.accumulator * num,
+            Operator::Cat => cat(self.accumulator, num),
+        };
+        self.accumulator = new;
     }
+}
+
+#[allow(dead_code)]
+fn print_eq(eq: &Equation, ops: &[&Operator]) {
+    let mut out = format!("{} = ", eq.result);
+    for x in eq
+        .numbers
+        .iter()
+        .map(|n| n.to_string())
+        .interleave(ops.iter().map(|o| o.to_string()))
+    {
+        out += " ";
+        out += &x;
+    }
+    println!("{}", out);
 }
 
 fn main() {
@@ -78,6 +108,8 @@ fn parse_input(input: &str) -> Vec<Equation> {
 fn check_eq(eq: Equation, allowed_ops: &[Operator]) -> bool {
     let num_ops = eq.numbers.len() - 1;
     for ops in (0..num_ops).map(|_| allowed_ops).multi_cartesian_product() {
+        //print!("Checking ");
+        //print_eq(&eq, &ops);
         let mut nums = eq.numbers.iter();
         let first = nums.next().unwrap();
         let mut cal = Calculator::new(*first);
@@ -85,7 +117,7 @@ fn check_eq(eq: Equation, allowed_ops: &[Operator]) -> bool {
             let right = nums.next().unwrap();
             cal.apply(**o, *right)
         }
-        if cal.finalize() == eq.result {
+        if cal.accumulator == eq.result {
             return true;
         }
     }
@@ -103,8 +135,15 @@ fn part1(input: Vec<Equation>) -> i64 {
     res
 }
 
-fn part2(_input: Vec<Equation>) -> u64 {
-    0
+fn part2(input: Vec<Equation>) -> i64 {
+    let mut res = 0;
+    for eq in input {
+        let eq_res = eq.result;
+        if check_eq(eq, &ALL_OPS) {
+            res += eq_res;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -132,6 +171,6 @@ mod test {
     fn test_part2() {
         let i = parse_input(INP);
         let res = part2(i);
-        assert_eq!(res, 0);
+        assert_eq!(res, 11387);
     }
 }
