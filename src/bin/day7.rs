1851c10ba615bebcae181ecc502cf0fc973ea77e
Ronald Weber <ronaldxweber@gmail.com>
2025-01-26 16:58:59 +0100

    Day 7 refactor

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -14,13 +14,39 @@ struct Equation {
     numbers: Vec<i64>,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Copy, Clone, Debug)]
 enum Operator {
     Add,
     Mul,
 }
 
 const ADD_MUL: [Operator; 2] = [Operator::Add, Operator::Mul];
+
+struct Calculator {
+    accumulator: i64,
+    prev: Option<(Operator, i64)>,
+}
+
+impl Calculator {
+    fn new(num: i64) -> Self {
+        Self { accumulator: num, prev: None }
+    }
+
+    fn apply(&mut self, op: Operator, num: i64) {
+        self.finalize();
+        self.prev = Some((op, num));
+    }
+
+    fn finalize(&mut self) -> i64 {
+        if let Some(prev) = self.prev {
+            self.accumulator = match prev.0 {
+                Operator::Add => self.accumulator + prev.1,
+                Operator::Mul => self.accumulator * prev.1,
+            };
+        }
+        self.accumulator
+    }
+}
 
 fn main() {
     let cli = Cli::parse();
@@ -49,26 +75,29 @@ fn parse_input(input: &str) -> Vec<Equation> {
     res
 }
 
+fn check_eq(eq: Equation, allowed_ops: &[Operator]) -> bool {
+    let num_ops = eq.numbers.len() - 1;
+    for ops in (0..num_ops).map(|_| allowed_ops).multi_cartesian_product() {
+        let mut nums = eq.numbers.iter();
+        let first = nums.next().unwrap();
+        let mut cal = Calculator::new(*first);
+        for o in ops.iter() {
+            let right = nums.next().unwrap();
+            cal.apply(**o, *right)
+        }
+        if cal.finalize() == eq.result {
+            return true;
+        }
+    }
+    false
+}
+
 fn part1(input: Vec<Equation>) -> i64 {
     let mut res = 0;
     for eq in input {
-        let num_ops = eq.numbers.len() - 1;
-        for ops in (0..num_ops).map(|_| ADD_MUL).multi_cartesian_product() {
-            let mut nums = eq.numbers.iter();
-            let mut left = nums.next().unwrap();
-            let mut acc = 0;
-            for o in ops.iter() {
-                let right = nums.next().unwrap();
-                acc = match o {
-                    Operator::Add => left + right,
-                    Operator::Mul => left * right,
-                };
-                left = &acc;
-            }
-            if acc == eq.result {
-                res += acc;
-                break;
-            }
+        let eq_res = eq.result;
+        if check_eq(eq, &ADD_MUL) {
+            res += eq_res;
         }
     }
     res
